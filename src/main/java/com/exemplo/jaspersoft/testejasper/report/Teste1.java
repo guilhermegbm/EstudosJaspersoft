d324170741cced4c15899c3ff3f1c6d350054ddd
Mariana Eliza Alves Costa <68155516+xMarih@users.noreply.github.com>
2021-09-20 18:19:12 -0300

    alteração ao chamar o metodo printDecompiledReportWithEmptyDataSource

--- src/main/java/com/exemplo/jaspersoft/testejasper/report/Teste1.java
+++ src/main/java/com/exemplo/jaspersoft/testejasper/report/Teste1.java
@@ -7,6 +7,6 @@ public class Teste1 {
 
 	public void gerarRelatorioTeste1() throws Exception {
 //		RelatorioPdfUtil.printReportWithEmptyDataSource("C:\\Users\\guilh\\Desktop\\reports_gerados\\Teste1.pdf", null, "/relatorios/teste1_exemploEstatico.jasper");
-		RelatorioPdfUtil.printReportWithEmptyDataSource2("C:\\Users\\marry\\Desktop\\reports_gerados\\Teste1.pdf", null, "C:\\Users\\marry\\Documents\\apis\\testejasper\\src\\main\\resources\\relatorios\\teste1_exemploEstatico.jrxml");
+		RelatorioPdfUtil.printDecompiledReportWithEmptyDataSource("C:\\Users\\marry\\Desktop\\reports_gerados\\Teste1.pdf", null, "C:\\Users\\marry\\Documents\\apis\\testejasper\\src\\main\\resources\\relatorios\\teste1_exemploEstatico.jrxml");
 	}
 }
