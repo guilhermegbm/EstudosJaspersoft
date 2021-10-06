d787af4a3a82e3fdbdaf2fa70a4b8878c41e1c30
Mariana Eliza Alves Costa <68155516+xMarih@users.noreply.github.com>
2021-10-06 17:29:54 -0300

    implementação VO e passagem de paremantros

---
+++ src/main/java/com/exemplo/jaspersoft/testejasper/vo/RelatorioExtratoOnlineVO.java
@@ -0,0 +1,22 @@
+package com.exemplo.jaspersoft.testejasper.vo;
+
+import java.util.List;
+
+import com.exemplo.jaspersoft.testejasper.entity.ExtratoMensal;
+import com.exemplo.jaspersoft.testejasper.entity.Pessoa;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+
+public class RelatorioExtratoOnlineVO {
+	private Pessoa valoresPorPessoaEMes;
+	private List<String> periodos;
+	private ExtratoMensal extratoMensal;
+
+}
+//DTO - Data Transfer Object 
+//VO  - Virtual Object
+//
